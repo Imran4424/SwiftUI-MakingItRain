cf089f5cea8398fbe48c5069dc56dcd3c34a1c13
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-02-18 00:39:18 +0600

    content view done

--- MakingItRain/ContentView.swift
+++ MakingItRain/ContentView.swift
@@ -9,16 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ParticleView(createSystem()) {
+            Image(.circle)
+                .blendMode(.plusLighter)
+                .tag("unitView")
+            Text("🦄")
+                .font(.system(size: 150))
+                .tag("unicorn")
         }
-        .padding()
+    }
+}
+
+extension ContentView {
+    func createSystem() -> ParticleSystem {
+        let system = ParticleSystem()
+        system.tags = ["unitView"]
+        system.position = [0.5, 0]
+        system.angle = .degrees(180)
+        system.angleVary = .degrees(20)
+        system.shape = .box(width: 1, height: 0)
+        system.color = .green
+        system.speed = 0.2
+        system.speedVary = 0.4
+        system.acceleration = [0, 1]
+        system.size = 0.5
+        system.sizeVary = 1
+        system.sizeAtDeath = 0
+        
+        return system
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
