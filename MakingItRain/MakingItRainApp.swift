8da7cb983a1e1ba09872c13878c734f1be277f09
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-02-18 00:36:55 +0600

    project init

---
+++ MakingItRain/MakingItRainApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakingItRainApp.swift
+//  MakingItRain
+//
+//  Created by Shah Md Imran Hossain on 17/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MakingItRainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
